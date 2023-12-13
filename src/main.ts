1f49e8ef5fa188ee7357ebe6e85de5bb91819731
siavava <amittaijoel@outlook.com>
2023-12-13 04:42:05 -0500

    update app

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import routes from 'pages-generated'
 
-import App from './App.vue'
+import App from './app.vue'
 
 const app = createApp(App)
 const router = createRouter({
